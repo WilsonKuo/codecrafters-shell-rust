2fe112e85a27370362d98a44f4ca5363e30eda6f
Wilson Kuo <WilsonKuo85@gmail.com>
2026-02-05 21:36:41 +0800

    Redirect stdout

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,15 @@ fn main() {
         let args: Vec<&str> = args.iter().map(|s| s.as_str()).collect();
         if let Some(cmd) = args.first() {
             match *cmd {
-                "echo" => println!("{}", args[1..].join(" ")),
+                "echo" => {
+                    if args.len() > 3 && matches!(args[2], ">" | "1>") {
+                        if let Err(e) = std::fs::write(&args[3], format!("{}\n", &args[1])) {
+                            println!("{:?}", e);
+                        };
+                    } else {
+                        println!("{}", args[1..].join(" "));
+                    }
+                }
                 "exit" => std::process::exit(0),
                 "type" => {
                     if let Some(cmd_name) = args.get(1) {
@@ -64,16 +72,41 @@ fn main() {
                 _ => {
                     if let Some(path_val) = std::env::var_os("PATH") {
                         let mut find: bool = false;
+                        let mut args2 = Vec::new();
+                        let mut file = String::new();
+
+                        let mut v_iter = args[1..].into_iter();
+                        while let Some(arg) = v_iter.next() {
+                            if matches!(arg, &">" | &"1>") {
+                                if let Some(f) = v_iter.next() {
+                                    file = f.to_string();
+                                    break;
+                                }
+                            } else {
+                                args2.push(arg);
+                            }
+                        }
+
                         for path in std::env::split_paths(&path_val) {
                             let full_path = path.join(*cmd);
                             if is_executable::is_executable(&full_path) {
                                 find = true;
                                 let output = std::process::Command::new(*cmd)
-                                    .args(&args[1..])
+                                    .args(&args2)
                                     .output()
-                                    .expect("failed to execute process")
-                                    .stdout;
-                                print!("{}", String::from_utf8(output).expect("Not valid UTF-8"));
+                                    .expect("failed to execute process");
+                                let stdout =
+                                    String::from_utf8(output.stdout).expect("Not valid UTF-8");
+                                let stderr =
+                                    String::from_utf8(output.stderr).expect("Not valid UTF-8");
+                                if file.is_empty() {
+                                    print!("{}", stdout);
+                                } else {
+                                    if let Err(e) = std::fs::write(&file, &stdout) {
+                                        println!("{:?}", e);
+                                    }
+                                }
+                                print!("{}", stderr);
                                 break;
                             }
                         }
